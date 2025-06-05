c73c22f75bdc3c72b28492435f6c56956c0c8e77
gabrielzj <gabrielzuinj@gmail.com>
2025-06-04 23:21:03 -0300

    add: new routes url

--- crypto_app/urls.py
+++ crypto_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import IndexView, CreateUserView
-
+from .views import IndexView, CreateUserView, ListUserView, RetrieveUserView, UpdateUserView, DeleteUserView
 urlpatterns = [
-    path("", IndexView.as_view(), name='index'), # as_view() is used to link the class-based view into the URL
-    path("users/", CreateUserView.as_view(), name='user_list'),  # Example for a user list view
+    path("", IndexView.as_view(), name='index'), # as_view() linka uma classe de view a uma URL
+    path("users/create", CreateUserView.as_view(), name='create_user'),
+    path("users/list", ListUserView.as_view(), name='list_user'),
+    path("users/<int:pk>/retrieve", RetrieveUserView.as_view(), name='retrieve_user'),
+    path("users/<int:pk>/update", UpdateUserView.as_view(), name='update_user'),
+    path("users/<int:pk>/delete", DeleteUserView.as_view(), name='delete_user'),
 ]
