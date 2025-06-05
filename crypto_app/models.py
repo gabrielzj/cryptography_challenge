6510ef16eacc48347bf5cdb746c6f7f9cdc1787a
gabrielzj <gabrielzuinj@gmail.com>
2025-06-04 23:20:07 -0300

    alter model field 'userDocument', 'creditCardToken' and 'value'

--- crypto_app/models.py
+++ crypto_app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    userDocument = models.CharField(max_length=255, unique=True)
-    creditCardToken = models.CharField(max_length=255, unique=True)
-    value = models.DecimalField(max_digits=10, decimal_places=2)
+    userDocument = models.CharField(max_length=255, unique=True, null=False)
+    creditCardToken = models.CharField(max_length=255, unique=True, null=False)
+    value = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     
