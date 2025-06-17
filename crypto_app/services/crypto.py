c1d92258dcec7aa7eda9cb196eb2fc0b33a386a3
gabrielzj <gabrielzuinj@gmail.com>
2025-06-17 01:57:58 -0300

    refactor cryptography service

--- crypto_app/services/crypto.py
+++ crypto_app/services/crypto.py
@@ -5,9 +5,22 @@ from cryptography.fernet import Fernet
 key = os.environ["FERNET_SECRET_KEY"].encode()
 f = Fernet(key)
 
-def encrypt(inf, **kwargs):
-    token = f.encrypt(inf)
-    return token
+# def encrypt(inf, **kwargs):
+#     token = f.encrypt(inf)
+#     return token
 
-def decrypt(token, **kwargs):
-    return f.decrypt(token)
+# def decrypt(token, **kwargs):
+#     return f.decrypt(token)
+
+class CryptoService:
+    def __init__(self, key: str = None):
+        key = key or os.environ["FERNET_SECRET_KEY"]
+        # posso ter múltiplos objetos CryptoService, por isso indicar o obj dessa instância
+        # permite que o obj 'fernet' exista fora do construtor
+        self.fernet = Fernet(key.encode())
+        
+    def encrypt(self, data: bytes) -> bytes:
+        return self.fernet.encrypt(data)
+    
+    def decrypt(self, token: bytes) -> bytes:
+        return self.fernet.decrypt(token)
