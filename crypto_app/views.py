24099e6c5575c7e9428cafa3223f15f795e49e58
gabrielzj <gabrielzuinj@gmail.com>
2025-06-25 00:14:35 -0300

    refactor: serializer argument to update request types

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -38,7 +38,6 @@ def list_user(request):
 def retrieve_user(request, pk):
     try:
         queryset = User.objects.get(pk=pk)
-        # get_object_or_404(queryset)
         serializer = UserSerializer(queryset)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
@@ -48,10 +47,17 @@ def retrieve_user(request, pk):
 def update_user(request, pk):
     try:
         queryset = User.objects.get(pk=pk)
-        serializer = UserSerializer(instance=queryset, data=request.data)
+        # partial = request.method == 'PATCH'
+        if request.method == 'PATCH':
+            serializer = UserSerializer(queryset, data=request.data, partial=True)
+        elif request.method == 'PUT':
+            serializer = UserSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        return Response({'error': 'Usuário não existe'}, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
@@ -60,7 +66,6 @@ def delete_user(request, pk):
     try:
         queryset = User.objects.get(pk=pk)
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"User deletado com sucesso"}, status=status.HTTP_202_ACCEPTED)
     except User.DoesNotExist:
         return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-    
